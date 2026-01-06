2313673391f1b7c667e60d466478965b69b283a5
nguyennguyen777 <hanhnguyenhpt1502@gmail.com>
2026-01-06 17:36:00 +0700

    Cập nhật form admin cho phép chỉnh sửa nội dung chi tiết sản phẩm

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -89,26 +89,23 @@
 
                 <div class="form-group">
                     <label for="description">Mô tả (Tiếng Việt)</label>
-                    <textarea name="description" 
-                              id="description" 
-                              class="form-control" 
-                              rows="8">{{ old('description') }}</textarea>
+                    <textarea name="description" id="editor">
+        {{ old('description', $product->description ?? '') }}
+    </textarea>
                 </div>
 
                 <div class="form-group">
                     <label for="description_en">Mô tả (Tiếng Anh)</label>
-                    <textarea name="description_en" 
-                              id="description_en" 
-                              class="form-control" 
-                              rows="8">{{ old('description_en') }}</textarea>
+                    <textarea name="description" id="editor">
+        {{ old('description', $product->description ?? '') }}
+    </textarea>
                 </div>
 
                 <div class="form-group">
                     <label for="description_zh">Mô tả (Tiếng Trung)</label>
-                    <textarea name="description_zh" 
-                              id="description_zh" 
-                              class="form-control" 
-                              rows="8">{{ old('description_zh') }}</textarea>
+                    <textarea name="description" id="editor">
+        {{ old('description', $product->description ?? '') }}
+    </textarea>
                 </div>
             </div>
         </div>
@@ -193,6 +190,7 @@
 </div>
 
 @push('scripts')
+<script src="https://cdn.ckeditor.com/4.22.1/full/ckeditor.js"></script>
 <script>
 function previewImage(input, previewId) {
     if (input.files && input.files[0]) {
@@ -205,6 +203,11 @@ function previewImage(input, previewId) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+CKEDITOR.replace('editor', {
+        height: 400,
+        extraAllowedContent: 'img(*)'
+    });
 </script>
 @endpush
 @endsection
